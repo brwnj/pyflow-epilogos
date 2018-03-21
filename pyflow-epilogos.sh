c85d4ae077c0ca2e93282a02c335ff7f352ac787
Joe Brown <brwnjm@gmail.com>
2018-03-21 12:43:02 -0700

    updates sh cmd

--- pyflow-epilogos.sh
+++ pyflow-epilogos.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 <<doc
+snakemake --rerun-incomplete -j 1000 --jobscript ./jobscript.sh \
+		  --timestamp --latency-wait 240 \
+		  --cluster-config cluster.json --cluster './bsub_cluster.py' \
+		  "$@"
+doc
+
+# invoke Snakemake in cluster mode with custom wrapper scripts
+
 snakemake --jobs 99999 \
     --timestamp \
     --localcores 24 \
     --latency-wait 240 \
     --cluster-config cluster.json \
-    --cluster "sbatch --account {cluster.account} --partition {cluster.partition} --exclusive --nodes 1 --ntasks 24 --time {cluster.time} --job-name {cluster.name} --output {cluster.output} --error {cluster.error}"
-doc
-
-# invoke Snakemake in cluster mode with custom wrapper scripts
-
-snakemake --rerun-incomplete -j 1000 --jobscript ./jobscript.sh \
-		  --timestamp --latency-wait 240 \
-		  --cluster-config cluster.json --cluster './bsub_cluster.py' \
-		  "$@"
+    --cluster "sbatch --account {cluster.account} --partition {cluster.partition} --exclusive --nodes 1 --ntasks 24 --time {cluster.time} --job-name {cluster.name} --output {cluster.output} --error {cluster.error}" \
+    09vsurf_output_by_chr/chr22_vsurf_out.rda
